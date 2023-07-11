f4bd1df5a5dbdb3f733bca33f6e1a97ee3cc50c9
TheBlackHole <XXXtradiator@gmail.com>
2023-07-11 18:03:57 +0200

    add some animations and pop up

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const items = document.getElementById('items')
+const addItem = document.getElementById('add-item')
+
+function addAnItem() {
+	items.appendChild(document.createElement('li'))
+}
+
+addItem.addEventListener('click', event => {
+	addAnItem()
+})
